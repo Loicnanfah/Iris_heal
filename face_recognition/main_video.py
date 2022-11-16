224b14dbb2c9e491b3d260fec5d994878219b180
loicobg <yayzoy@gmail.com>
2022-11-16 02:50:38 -0600

    add

---
+++ face_recognition/main_video.py
@@ -0,0 +1,32 @@
+import cv2
+from simple_facerec import SimpleFacerec
+
+# Encode faces from a folder
+sfr = SimpleFacerec()
+sfr.load_encoding_images("images/")
+
+# Load Camera
+cap = cv2.VideoCapture(0)
+
+
+while True:
+    ret, frame = cap.read()
+
+    # Detect Faces
+    face_locations, face_names = sfr.detect_known_faces(frame)
+
+
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 1)
+        cv2.rectangle(frame, (left, bottom - 20), (right, bottom), (0, 255, 0), cv2.FILLED)
+        cv2.putText(frame, name, (left + 2, bottom - 2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
